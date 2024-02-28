bc6d8f301abb0f4171f5d90a2590e78c9a97e440
Simon Paul <samonpaul06@gmail.com>
2024-02-28 14:36:35 +0530

    initial: whiteboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,131 @@
-import Image from 'next/image'
+"use client"
+import { MouseEvent, useEffect, useRef, useState } from 'react'
 import styles from './page.module.css'
+import { FaEraser, FaPencilAlt } from 'react-icons/fa';
 
 export default function Home() {
+
+  const [canvasContext, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
+  const [mouseDown, setMouseDown] = useState<Boolean>(false); // false - mouseup, true - mousedown 
+  const [startPoint, setStartPoint] = useState({ x: 0, y: 0 });
+  // const [lineWidth, setLineWidth] = useState<number>(10);
+
+
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const handleMouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
+    setMouseDown(true);
+    canvasContext?.beginPath();
+    let coordinates = { x: e.pageX, y: e.pageY };
+    setStartPoint(coordinates);
+  }
+
+  // convert coordinates 
+  const convertCoordinates = (x: number, y: number): { x: number, y: number } => {
+    const rect = canvasRef.current?.getBoundingClientRect()!;
+    const scaleX = (canvasRef.current?.width || 0) / rect?.width;
+    const scaleY = (canvasRef.current?.height || 0) / rect.height;
+
+    return {
+      x: (x - rect.left) * scaleX,
+      y: (y - rect.top) * scaleY
+    };
+  }
+
+  const handleMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
+    if (mouseDown) {
+      let coordinates = { x: e.pageX, y: e.pageY };
+      let prevCoordinates: { x: number, y: number } = convertCoordinates(startPoint.x, startPoint.y);
+      let currentCoordinates: { x: number, y: number } = convertCoordinates(e.pageX, e.pageY);
+      canvasContext?.moveTo(prevCoordinates.x, prevCoordinates.y);
+      canvasContext?.lineTo(currentCoordinates.x, currentCoordinates.y);
+      canvasContext?.closePath();
+      canvasContext?.stroke();
+      setStartPoint(coordinates);
+    }
+  }
+
+  const handleMouseUp = () => {
+    setMouseDown(false);
+    canvasContext?.closePath();
+    canvasContext?.stroke();
+  }
+
+  // handle stroke color 
+  const handleStrokeColor = (color: string) => {
+    if (canvasContext) {
+      canvasContext.strokeStyle = color
+    }
+  }
+
+  // handle tool change
+  const handleToolChange = (tool: string) => {
+    if(canvasContext){
+      if(tool === "eraser"){
+        canvasContext.globalCompositeOperation = 'destination-out'
+      }else if(tool === "pencil"){
+        canvasContext.globalCompositeOperation = 'source-over'
+      }
+    }
+  }
+
+  // // handle line width
+  // const handleLineWidth = (width: number) => {
+  //   if(canvasContext){
+  //     setLineWidth(() => {
+  //       canvasContext.lineWidth = width;
+  //       return width
+  //     })
+  //   }
+  // }
+
+  const resetCanvas = () => {
+    canvasContext?.reset();
+  }
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d')!;
+    setCanvasContext(ctx);
+  }, [])
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className={styles.container}>
+
+        <div className={styles.canvasWrapper}>
+          <div className={styles.canvasTools}>
+            <div className={styles.canvasTool} title='Pencil' onClick={() => handleToolChange('pencil')}>
+              <FaPencilAlt className={styles.canvasToolIcon} color='#fff'/>
+            </div>
+            <div className={styles.canvasTool} title='Eraser' onClick={() => handleToolChange('eraser')}>
+              <FaEraser className={styles.canvasToolIcon} color='#fff'/>
+            </div>
+          </div>
+          <canvas
+            className={styles.canvas}
+            width={1100}
+            height={450}
+            ref={canvasRef}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          </canvas>
+        </div>
+        <div className={styles.toolsContainer}>
+          <div className={styles.colorSet}>
+            <div className={`${styles.color} ${styles.red}`} onClick={() => handleStrokeColor('red')}></div>
+            <div className={`${styles.color} ${styles.black}`} onClick={() => handleStrokeColor('black')}></div>
+            <div className={`${styles.color} ${styles.blue}`} onClick={() => handleStrokeColor('blue')}></div>
+          </div>
+          <div className={styles.btns}>
+            {/* <input type="range" value={lineWidth} min={10} max={50} onChange={(e) => handleLineWidth(Number(e.target.value))}/> */}
+            <button className={styles.reset} onClick={resetCanvas}>Reset</button>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
